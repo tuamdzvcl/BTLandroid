27b3978cefa047b5e69d27af70f9495a5087d8c6
tuamdzvcl <phamngoctuananh161@gmail.com>
2024-04-09 16:38:41 +0700

    first commit

---
+++ app/src/main/java/com/example/btl/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.btl;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.SQLData;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editmasv,edittensv,editquequan;
+    Button btninsert,btnupdate,btndelete,btnsearch;
+    public void addcontroler(){
+
+        editmasv = findViewById(R.id.edtmasv);
+        edittensv = findViewById(R.id.edttensv);
+        editquequan = findViewById(R.id.edtquequan);
+        btninsert = findViewById(R.id.btninsert);
+        btndelete = findViewById(R.id.btndelete);
+        btnupdate = findViewById(R.id.btnUpdate);
+        btnsearch = findViewById(R.id.btnsearch);
+    };
+
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        addcontroler();
+        db = SQLiteDatabase.openDatabase("data/data/com.example.btl/QuanlySinhVien",null,SQLiteDatabase.CREATE_IF_NECESSARY);
+        db.close();
+    }
+
+    public void insert(View view) {
+        String masv = editmasv.getText().toString();
+        String tensv = edittensv.getText().toString();
+        String quequan = editquequan.getText().toString();
+        String msv = new String(masv);
+        String tsv = new String(tensv);
+        String qq = new String(quequan);
+
+        Toast.makeText(MainActivity.this,msv+"-"+tsv+"-"+qq,Toast.LENGTH_SHORT).show();
+    }
+}
